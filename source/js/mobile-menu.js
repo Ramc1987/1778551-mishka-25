cfd97985496716f109019cdd054ae289512482b6
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-05-10 23:51:02 +0300

    Add menu js

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -1,10 +1,17 @@
 let btn = document.querySelector('.header__toggle-menu');
 let siteList = document.querySelector('.header__site-list');
+let userList = document.querySelector('.header__user-list')
+// siteList.classList.remove('.header__site-list--open')
 
 btn.addEventListener('click', function() {
-  if (siteList.classList.toggle('.header__site-list--open')) {
+  if (siteList.classList.toggle('header__site-list--open')) {
     siteList.classList.remove('header__site-list--close')
   } else {
     siteList.classList.add('header__site-list--close');
+  }
+  if (userList.classList.toggle('header__user-list--open')) {
+    userList.classList.remove('header__user-list--close')
+  } else {
+    userList.classList.add('header__user-list--close');
   }
 })
