70999e677e0f7f808bf85f3ce6f052369fea61e6
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-05-10 22:38:27 +0300

    Add js in button-menu

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -1,5 +1,10 @@
 let btn = document.querySelector('.header__toggle-menu');
 let siteList = document.querySelector('.header__site-list');
+
 btn.addEventListener('click', function() {
-  siteList.classList.toggle('.header__site-list--open');
+  if (siteList.classList.toggle('.header__site-list--open')) {
+    siteList.classList.remove('header__site-list--close')
+  } else {
+    siteList.classList.add('header__site-list--close');
+  }
 })
