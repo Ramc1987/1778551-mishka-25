b4b89968c2205406385d998e384e30c0f45a0729
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-05-12 07:57:29 +0300

    Fix modal window

---
+++ source/js/modal.js
@@ -0,0 +1,20 @@
+let weeksBtn = document.querySelector('.top-product__button')
+let headerBtn = document.querySelector('.header__user-item')
+let modalWindow = document.querySelector('.modal')
+
+weeksBtn.addEventListener('click', function() {
+  if (modalWindow.classList.toggle('modal--open')) {
+    modalWindow.classList.remove('modal--close')
+  } else {
+    modalWindow.classList.add('modal--close');
+  }
+})
+
+headerBtn.addEventListener('click', function() {
+  if (modalWindow.classList.toggle('modal--open')) {
+    modalWindow.classList.remove('modal--close')
+  } else {
+    modalWindow.classList.add('modal--close');
+  }
+})
+
