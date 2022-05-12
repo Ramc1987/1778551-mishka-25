a80c865205a8cfa2f93abfa0329cb72ae3df0283
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-05-13 00:44:24 +0300

    Add focus in page form and add js in modal-window

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -1,7 +1,7 @@
 let btn = document.querySelector('.header__toggle-menu');
 let siteList = document.querySelector('.header__site-list');
 let userList = document.querySelector('.header__user-list')
-// siteList.classList.remove('.header__site-list--open')
+
 
 btn.addEventListener('click', function() {
   if (siteList.classList.toggle('header__site-list--open')) {
--- source/js/modal.js
+++ source/js/modal.js
@@ -1,8 +1,11 @@
 let weeksBtn = document.querySelector('.top-product__button')
-let headerBtn = document.querySelector('.header__user-item')
 let modalWindow = document.querySelector('.modal')
+let modalForm = document.querySelector('.modal__window')
+let headerBtn = document.querySelector('.header__user-item--basket');
+let productBaskets = document.querySelectorAll('.card-list__wrapper-basket')
+console.log(headerBtn);
 
-weeksBtn.addEventListener('click', function() {
+weeksBtn?.addEventListener('click', function() {
   if (modalWindow.classList.toggle('modal--open')) {
     modalWindow.classList.remove('modal--close')
   } else {
@@ -10,13 +13,30 @@ weeksBtn.addEventListener('click', function() {
   }
 })
 
+modalWindow.addEventListener('click', function() {
+  modalWindow.classList.remove('modal--open');
+  modalWindow.classList.add('modal--close');
+})
 
+modalForm.addEventListener('click', function(event) {
+  event.stopPropagation();
+})
 
-// headerBtn.addEventListener('click', function() {
-//   if (modalWindow.classList.toggle('modal--open')) {
-//     modalWindow.classList.remove('modal--close')
-//   } else {
-//     modalWindow.classList.add('modal--close');
-//   }
-// })
+headerBtn?.addEventListener('click', function () {
+  if (modalWindow.classList.toggle('modal--open')) {
+    modalWindow.classList.remove('modal--close')
+  } else {
+    modalWindow.classList.add('modal--close');
+  }
+})
 
+productBaskets.forEach(basket => {
+  basket.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (modalWindow.classList.toggle('modal--open')) {
+      modalWindow.classList.remove('modal--close')
+    } else {
+      modalWindow.classList.add('modal--close');
+    }
+  })
+});
